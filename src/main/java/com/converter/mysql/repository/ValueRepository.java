5a98176977f1d740505bbb43a6b045b62c27b57b
andrey <ondreychikishev@gmail.com>
2018-08-20 11:32:48 +0300

    Edit application.properties

--- src/main/java/com/converter/mysql/repository/ValueRepository.java
+++ src/main/java/com/converter/mysql/repository/ValueRepository.java
@@ -4,7 +4,7 @@ import com.converter.mysql.entity.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "/index")
+@RepositoryRestResource(path = "/value")
 public interface ValueRepository extends JpaRepository<Value, String> {
 
 }
